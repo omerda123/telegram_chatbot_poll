afed25d2f3acac173f296e45aaf885a58a883a2b
Omer Daniel <omerda@gmail.com>
2020-01-22 22:39:21 +0200

    add insert to mongoDB

--- main.py
+++ main.py
@@ -18,56 +18,86 @@ updater = Updater(token=bot_settings.BOT_TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
 
+def cancel(update: Update, context: CallbackContext):
+    context.bot.send_message(chat_id=update.message.chat_id, text="Goodbye")
+
+
 def start(update: Update, context: CallbackContext):
     chat_id = update.effective_chat.id
     logger.info(f"> Start chat #{chat_id}")
     custom_keyboard = ["כן", "לא"],
     reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
-    context.bot.send_message(chat_id=update.message.chat_id,
-                             text=""" 
+    update.message.reply_text(""" 
                              היי, האם תרצה להשתתף בסקר קצר לקראת הבחירות?
                                ניתן לצאת באמצעות /cancel
                            """,
-                             reply_markup=reply_markup)
+                              reply_markup=reply_markup)
 
     return AGE
 
 
-def cancel(update: Update, context: CallbackContext):
-    context.bot.send_message(chat_id=update.message.chat_id, text="Goodbye")
-
-
-AGE, GENDER, CITY, PARTY = range(4)
+AGE, GENDER, CITY, PARTY, FINISH = range(5)
 
 
 def age(update: Update, context: CallbackContext):
+    text = update.message.text
+    res[str(update.effective_chat.id)]= {
+        "name": update.effective_chat.first_name,
+        "lname": update.effective_chat.last_name,
+        "want": text
+    }
     custom_keyboard = ["18-30", "30-40", "40-50", "50-60"],
     reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
-    context.bot.send_message(chat_id=update.message.chat_id, text="בן כמה אתה?",
-                             reply_markup=reply_markup)
+    update.message.reply_text("בן כמה אתה?", reply_markup=reply_markup)
     return GENDER
 
 
 def gender(update: Update, context: CallbackContext):
+    text = update.message.text
+    res[str(update.effective_chat.id)].update ({
+        "age": text
+    })
+
     custom_keyboard = ["זכר", "נקבה", "אחר"],
     reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
-    context.bot.send_message(chat_id=update.message.chat_id, text="מה המין שלך?",
-                             reply_markup=reply_markup)
+    update.message.reply_text("מה המין שלך?", reply_markup=reply_markup)
     return CITY
 
 
 def city(update: Update, context: CallbackContext):
     text = update.message.text
-    context.bot.send_message(chat_id=update.message.chat_id, text="מהי עיר מגוריך?", reply_markup=ReplyKeyboardRemove())
+    res[str(update.effective_chat.id)].update({
+        "gender": text
+    })
+    update.message.reply_text("מהי עיר מגוריך?", reply_markup=ReplyKeyboardRemove())
 
     return PARTY
 
 
 def party(update: Update, context: CallbackContext):
+    text = update.message.text
+    res[str(update.effective_chat.id)].update({
+        "city": text
+    })
     custom_keyboard = ["ליכוד", "כחול לבן", "שס", "ימינה"],
     reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
-    context.bot.send_message(chat_id=update.message.chat_id, text="איזה מפלגה את\ה מתכוון לבחור?",
-                             reply_markup=reply_markup)
+    update.message.reply_text("באיזה מפלגה את\ה מתכוון לבחור?",
+                              reply_markup=reply_markup)
+
+    return FINISH
+
+
+def finish(update: Update, context: CallbackContext):
+    text = update.message.text
+    res[str(update.effective_chat.id)].update({
+        "party": text
+    })
+    client = MongoClient()
+    db = client.get_database("survey")
+    results = db.get_collection("results")
+    results.insert_one(res)
+    print(res)
+    res.clear()
     return ConversationHandler.END
 
 
@@ -82,16 +112,16 @@ conv_handler = ConversationHandler(
 
         CITY: [MessageHandler(Filters.text, city)],
 
-        PARTY: [MessageHandler(Filters.text, party)]
+        PARTY: [MessageHandler(Filters.text, party)],
+
+        FINISH: [MessageHandler(Filters.text, finish)],
     },
 
     fallbacks=[CommandHandler('cancel', cancel)]
 )
 
-# start_handler = CommandHandler('start', start)
-# dispatcher.add_handler(start_handler)
 dispatcher.add_handler(conv_handler)
-
+res = {}
 logger.info("* Start polling...")
 updater.start_polling()  # Starts polling in a background thread.
 updater.idle()  # Wait until Ctrl+C is pressed
